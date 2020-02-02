ab7c0b9592acbc906aabaa0f6c960be8b984f846
ShahHarsh21 <harshshah942737@gmail.com>
2020-02-02 11:02:30 +0530

    2nd feb 2020

--- models/slot_models.js
+++ models/slot_models.js
@@ -18,7 +18,7 @@ var slot={
     },
     updateSlot:function(item,callback)
     {
-        return db.query('update slot_tbl set vehicle_type=?,vehicle_model,service_type=?,time_period=?,pickup_time=?,pickup_address=?,requirment=?,drop_address=?,alloted_emp_id=? where slot_register_id=?',[item.vehicle_type,item.vehicle_model,item.service_type,item.time_period,item.pickup_time,item.pickup_address,item.requirment,item.drop_address,item.alloted_employee_id,item.slot_registered_id],callback);
+        return db.query('update slot_tbl set vehicle_type=?,vehicle_model=?,service_type=?,time_period=?,pickup_time=?,pickup_address=?,requirment=?,drop_address=?,alloted_emp_id=? where slot_register_id=?',[item.vehicle_type,item.vehicle_model,item.service_type,item.time_period,item.pickup_time,item.pickup_address,item.requirment,item.drop_address,item.alloted_employee_id,item.slot_registered_id],callback);
     }
 }
 module.exports=slot;
